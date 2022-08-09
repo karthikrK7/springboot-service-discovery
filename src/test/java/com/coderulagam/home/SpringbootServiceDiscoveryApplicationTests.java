4fe3e0dd3b1f4653bda8fb119186846b02b39218
karthikeyan kesavan <karthikeyan.kesavanr@gmail.com>
2022-08-09 11:03:45 +0530

    initial commit

---
+++ src/test/java/com/coderulagam/home/SpringbootServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderulagam.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
